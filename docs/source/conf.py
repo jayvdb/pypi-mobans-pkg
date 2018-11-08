c16b17029d9db20edf11ba326e07c624a4e161b2
chfw <wangc_2011@hotmail.com>
2018-11-08 07:59:26 +0000

    :egg: :ferris_wheel: release 0.0.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,9 +28,9 @@ copyright = u'2018 Onni Software Ltd.'
 author = u'C.W.'
 
 # The short X.Y version
-version = u'0.0.1'
+version = u'0.0.2'
 # The full version, including alpha/beta/rc tags
-release = u'0.0.1'
+release = u'0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
